27386cd6468d1832f07a1f9cb89d8a6863f2df5f
THE ENZO <enzo9338@yahoo.com>
2017-05-21 10:37:49 -0700

    Finished update and finished stick dude

---
+++ texMrSpace/texMrSpace/texMrSpace/smashthing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+namespace texMrSpace
+{
+    class Smashthing : Sprite
+    {
+        public Smashthing(Vector2 position,Texture2D image,Color tint)
+            :base(position,image,tint)
+        {
+
+        }
+    }
+}
